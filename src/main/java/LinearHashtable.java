8db795c5ba0366dbbc4929bd7a023451c2fe8248
Randy J Fortier <randy.fortier@uoit.ca>
2020-08-31 19:08:25 -0400

    Initial commit

---
+++ src/main/java/LinearHashtable.java
@@ -0,0 +1,11 @@
+public class LinearHashtable<T> extends Hashtable<T> {
+    public LinearHashtable(int capacity) {
+        super(capacity);
+    }
+
+    public int rehash(int previousHash) {
+
+        // YOUR CODE GOES HERE
+        
+    }
+}
---
+++ src/main/java/QuadraticHashtable.java
@@ -0,0 +1,11 @@
+public class QuadraticHashtable<T> extends Hashtable<T> {
+    public QuadraticHashtable(int capacity) {
+        super(capacity);
+    }
+
+    public int rehash(int previousHash) {
+
+        // YOUR CODE GOES HERE
+        
+    }
+}
