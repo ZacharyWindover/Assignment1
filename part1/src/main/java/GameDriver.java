891f1f12b41dd3af02d11200f58aba32b53cf77e
ZacharyWindover <71024596+ZacharyWindover@users.noreply.github.com>
2020-10-19 22:33:28 -0400

    Update GameDriver.java
    I made a few minor changes. Changed playerMove and enemyMove move variables instead of int variables because it makes it more efficient

--- part1/src/main/java/GameDriver.java
+++ part1/src/main/java/GameDriver.java
@@ -1,28 +1,45 @@
+package Game;
+
 public class GameDriver {
 	public static void main(String[] args) {
+		
+		// declare variables
+		
+		// define moves for player monster
 		Move move1 = new Move("Vine Whip", "Grass", 45, 1.0f);
 		Move move2 = new Move("Tackle", "Normal", 50, 1.0f);
 		Move move3 = new Move("Take Down", "Normal", 90, 0.85f);
 		Move move4 = new Move("Razor Leaf", "Grass", 55, 0.95f);
+		
+		// define player monster
 		Monster monster = new Monster("Bulbasaur", "Grass", 240, 45, 49, 49, move1, move2, move3, move4);
+		
+		// something
 		HumanPlayer player = new HumanPlayer(monster);
 		
+		// define moves for cpu monster
 		move1 = new Move("Scratch", "Normal", 40, 1.0f);
 		move2 = new Move("Ember", "Fire", 40, 1.0f);
 		move3 = new Move("Peck", "Flying", 35, 1.0f);
 		move4 = new Move("Fire Spin", "Fire", 35, 0.85f);
+		
+		// define cpu monster
 		monster = new Monster("Torchic", "Fire", 240, 45, 60, 40, move1, move2, move3, move4);
+		
+		// something
 		CPUPlayer enemy = new CPUPlayer(monster);
 
+		// main battle loop
 		while ((!player.hasLost()) && (!enemy.hasLost())) {
 			// print both monsters' HP
 			System.out.println("");
 			System.out.printf("%s has %d HP\n", player.getMonster().getName(), player.getMonster().getHP());
-			System.out.printf("%s has %d HP\n", enemy.getMonster().getName(), enemy.getMonster().getHP());
+			System.out.printf("%s has %d HP\n\n", enemy.getMonster().getName(), enemy.getMonster().getHP());
 			
 			// decide the next move
-			int playerMove = player.chooseMove();
-			int enemyMove = enemy.chooseMove();
+			// I changed these from int variables to Move object variables because it makes the program more efficient
+			Move playerMove = player.chooseMove();
+			Move enemyMove = enemy.chooseMove(); 
 			
 			// execute the next move
 			if (player.isFasterThan(enemy)) {
